016c1e5c1457bdde826615db1ce568038a4105e5
one-moonman <nicola.manoilov@gmail.com>
2022-10-01 23:13:45 +0200

    routes added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import express from "express";
-import { env } from "process";
+import morgan from "morgan";
+
+import router from "./routes";
 
 async function bootsrap() {
-    const app: express.Application = express();
-    app.listen(env.port, () => console.warn("App listening on port >>>>"))
+    const app = express();
+    app
+        .use(express.json())
+        .use(morgan('tiny'))
+        .use('/', router)
+        .use((error: any, _: any, res: express.Response, __: any) => {
+            return res
+                .status(error.statusCode || 500)
+                .json({
+                    message: error.message || "Internal server error",
+                    success: false
+                })
+        })
+        .listen(process.env.port || 3000, () => console.warn("Listening on port 3000"))
 }
+
 bootsrap()
